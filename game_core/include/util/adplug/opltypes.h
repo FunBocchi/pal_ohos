0292c8933a2448084d9ee42986e2574feca1148d
funbocchi <lzx13735999188@petalmail.com>
2026-04-14 00:08:48 +0800

    # 源代码向C++的移植

---
+++ game_core/include/util/adplug/opltypes.h
@@ -0,0 +1,28 @@
+//
+// Created on 2026/4/13.
+//
+// Node APIs are not fully supported. To solve the compilation error of the interface cannot be found,
+// please include "napi/native_api.h".
+
+#ifndef pal_ohos_OPLTYPES_H
+#define pal_ohos_OPLTYPES_H
+
+typedef enum _OPLCORE_TYPE {
+	OPLCORE_MAME,
+	OPLCORE_DBFLT,
+	OPLCORE_DBINT,
+	OPLCORE_NUKED,
+} OPLCORE_TYPE;
+
+typedef enum _OPLCHIP_TYPE {
+	OPLCHIP_OPL2,
+	OPLCHIP_OPL3,
+
+	OPLCHIP_DUAL_OPL2,
+} OPLCHIP_TYPE;
+
+#define OPL3_EXTREG_BASE    0x100
+#define OPL3_4OP_REGISTER   0x104
+#define OPL3_MODE_REGISTER  0x105
+
+#endif //pal_ohos_OPLTYPES_H
---
+++ game_core/include/util/pal_config.h
@@ -0,0 +1,27 @@
+/* -*- mode: c; tab-width: 4; c-basic-offset: 4; c-file-style: "linux" -*- */
+//
+// Copyright (c) 2009-2011, Wei Mingzhi <whistler_wmz@users.sf.net>.
+// Copyright (c) 2011-2024, SDLPAL development team.
+// All rights reserved.
+// C++ port and optimizations Copyright (c) 2025-2026 funbocchi <lzx13735999188@petalmail.com>
+// Based on the C version by Wei Mingzhi and SDLPAL development team.
+//
+// This file is part of SDLPAL.
+//
+// SDLPAL is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License, version 3
+// as published by the Free Software Foundation.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+#ifndef pal_ohos_PAL_CONFIG_H
+#define pal_ohos_PAL_CONFIG_H
+
+#endif //pal_ohos_PAL_CONFIG_H
