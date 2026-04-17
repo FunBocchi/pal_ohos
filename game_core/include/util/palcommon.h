6f812a2c2ed685b4f8148910a8dbad5e77eaf61d
funbocchi <lzx13735999188@petalmail.com>
2026-04-17 14:42:54 +0800

    # 移植了部分代码 - 添加了商品最高商品数

---
+++ game_core/include/util/palcommon.h
@@ -0,0 +1,31 @@
+/* -*- mode: c; tab-width: 4; c-basic-offset: 4; c-file-style: "linux" -*- */
+//
+// Copyright (c) 2009-2011, Wei Mingzhi <whistler_wmz@users.sf.net>.
+// Copyright (c) 2011-2024, SDLPAL development team.
+// All rights reserved.
+//
+// C++ port and optimizations Copyright (c) 2025-2026, FunBocchi <lzx13735999188@petalmail.com>
+//
+//
+// This file is part of SDLPAL.
+//
+// SDLPAL is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License, version 3
+// as published by the Free Software Foundation.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+#ifndef pal_ohos_PALCOMMON_H
+#define pal_ohos_PALCOMMON_H
+
+// 商店中的最大商品数
+#define MAX_STORE_ITEM 9
+
+#endif // pal_ohos_PALCOMMON_H
